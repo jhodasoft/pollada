54bdb97101f96d909cab09f5902919426a1bdc12
Jhonny Dante <jhodasoft@hotmail.com>
2025-09-21 07:26:31 -0500

    Solucionando el problema de CSRF

--- pollada_project/settings.py
+++ pollada_project/settings.py
@@ -127,3 +127,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_DOMAIN = '.up.railway.app'
+CSRF_COOKIE_DOMAIN = '.up.railway.app'
